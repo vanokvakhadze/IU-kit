fced4ededfb83c19728306392dcc3467369a5df6
vanokvakhadze <vkvakhadze35@gmail.com>
2024-04-03 01:59:31 +0400

    Initial Commit

---
+++ homework1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by vano Kvakhadze on 03.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
